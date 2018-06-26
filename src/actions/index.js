5527a5e817d8aa754a372b541e3f24d821cbc105
Robby Ackerley <rcackerley@me.com>
2018-06-25 20:43:29 -0400

    added unit tests and integration testing

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,18 @@
-import {SAVE_COMMENT} from 'actions/types';
+import {SAVE_COMMENT, FETCH_COMMENTS} from 'actions/types';
+import axios from 'axios'
 
 export let saveComment = (comment) => ({
   type: SAVE_COMMENT,
   payload: comment
 }
 )
+
+export let fetchComments = () => {
+  const response = axios.get(
+    'http://jsonplaceholder.typicode.com/comments'
+  )
+  return {
+    type: FETCH_COMMENTS,
+    payload: response
+  }
+}
--- src/components/__tests__/CommentList.test.js
+++ src/components/__tests__/CommentList.test.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import { mount } from 'enzyme';
-
 import CommentList from 'components/CommentList';
 import Root from 'Root';
 
 let wrapped;
 
 beforeEach(() => {
-  wrapped = mount(<Root><CommentList /></Root>)
+  const initialState = {
+    comments: ['Comment 1', 'Comment 2']
+  }
+  wrapped = mount(
+    <Root initialState={initialState}>
+      <CommentList />
+    </Root>)
 })
-it('creates on LI per comment', () => {
 
+it('creates on LI per comment', () => {
+  expect(wrapped.find('li').length).toEqual(2);
+})
+
+it('has a comment visible on the screen', () => {
+  expect(wrapped.render().text()).toContain('Comment 1')
+  expect(wrapped.render().text()).toContain('Comment 2')
 })
